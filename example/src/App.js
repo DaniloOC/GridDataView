91d3c55c541db3b3ef2f425e30c271591713769d
Danilo Cruz <danilo.oliveiradacruz@gmail.com>
2023-05-07 12:15:36 -0300

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'grid-data-view'
+import 'grid-data-view/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
